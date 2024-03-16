871f6531b5e9e8e6d6b662368ecc75c3566a3f0e
SHAIK ARIFULLA <111654622+heyshaikkk@users.noreply.github.com>
2024-03-16 20:51:43 +0530

    Update phishingdetection.py

--- phishingdetection.py
+++ phishingdetection.py
@@ -778,35 +778,3 @@ if st.button('Check Safety'):
 
 """
 
-# Commented out IPython magic to ensure Python compatibility.
-# %%writefile web_app.py
-# import streamlit as st
-# 
-# def predict_safety(url):
-#   x = np.array(obj.getFeaturesList(url)).reshape(1, 30)
-#   y_pred = gbc.predict(x)[0]
-#   return y_pred
-# 
-# 
-# st.title('Website Safety Checker')
-# 
-# 
-# url = st.text_input('Enter the URL:')
-# 
-# if st.button('Check Safety'):
-#     if not url:
-#         st.warning('Please enter a URL.')
-#     else:
-#         try:
-#             safety_prediction = predict_safety(url)
-#             if safety_prediction == 1:
-#                 st.success('We guess it is a safe website.')
-#             else:
-#                 st.error('Caution! Suspicious website detected.')
-#         except Exception as e:
-#             st.error(f'An error occurred: {str(e)}')
-
-import pyngrok
-
-ngrok authtoken 2dlsVFnF3ySuByBHcYmcv67YggX_5NYZ8u49RwasgVk3QW5LU
-
